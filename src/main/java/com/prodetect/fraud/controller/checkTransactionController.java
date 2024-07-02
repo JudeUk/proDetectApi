d2cb4b8db598fa5163cfc432f2e956e497d5f867
Judeukana@gmail.com <judeukana@Judes-MacBook-Pro.local>
2024-07-02 13:33:45 +0000

    added @Request params=false added

--- src/main/java/com/prodetect/fraud/controller/checkTransactionController.java
+++ src/main/java/com/prodetect/fraud/controller/checkTransactionController.java
@@ -139,7 +139,7 @@ public class checkTransactionController {
 
 
     @RequestMapping(value = "/getRiskScore", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Object> getRiskScore(@RequestBody String request,@RequestHeader HttpHeaders httpHeaders, final HttpServletRequest httpServletRequest) throws Exception {
+    public ResponseEntity<Object> getRiskScore(@RequestParam(required = false) String request,@RequestHeader HttpHeaders httpHeaders, final HttpServletRequest httpServletRequest) throws Exception {
         // MDC.put("requestId", utils.generateSessionId());
 //        String sourcecode = httpServletRequest.getHeader("x-source-code");
 //        CheckTransactionRequest req = new CheckTransactionRequest();
